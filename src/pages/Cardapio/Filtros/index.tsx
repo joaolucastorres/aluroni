006f6e5395637bc494d3ce9016da8a2bf7154b71
João Lucas <joaolucaspinheirotorres@gmail.com>
2023-06-24 21:49:27 -0300

    Conclusão do projeto

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,36 @@
+import filtros from './filtros.json'
+import styles from './Filtros.module.scss'
+import classNames from 'classnames'
+
+type IOpcao = typeof filtros[0]
+
+interface Props {
+  filtro: number | null,
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({ filtro, setFiltro }: Props) {
+  function selecionaFiltro(opcao: IOpcao) {
+    if (opcao.id === filtro) return setFiltro(null)
+    return setFiltro(opcao.id)
+  }
+
+  return (
+    <div className={styles.filtros}>
+      {
+        filtros.map(opcao => (
+          <button className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles["filtros__filtro--ativo"]]: opcao.id === filtro
+          })}
+            key={opcao.id}
+            onClick={() => selecionaFiltro(opcao)}>
+            {opcao.label}
+          </button>
+        )
+
+        )
+      }
+    </div>
+  )
+}
